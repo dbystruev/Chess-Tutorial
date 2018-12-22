e81f10731ff8074503f742e74588af80169a4987
Denis Bystruev <dbystruev@me.com>
2018-12-22 15:14:16 +0300

    Initial Commit

---
+++ Chess Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chess Tutorial
+//
+//  Created by Denis Bystruev on 22/12/2018.
+//  Copyright © 2018 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
